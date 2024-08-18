945d7823be5f94bcbbc830dbdbc8761f68922d2e
minhtungo <loducfc@gmail.com>
2024-08-18 00:04:40 -0400

    chore

--- src/app/[locale]/(public)/(public)/layout.tsx
+++ src/app/[locale]/(public)/(public)/layout.tsx
@@ -2,12 +2,16 @@ import AOSWrapper from "@/components/common/AOS";
 import MaxWidthWrapper from "@/components/common/MaxWidthWrapper";
 import Footer from "@/components/public/common/Footer";
 import Header from "@/components/public/common/Header";
+import { unstable_setRequestLocale } from "next-intl/server";
 
 export default function ChildPublicLayout({
   children,
+  params: { locale },
 }: Readonly<{
   children: React.ReactNode;
+  params: { locale: string };
 }>) {
+  unstable_setRequestLocale(locale);
   return (
     <AOSWrapper>
       <Header />
--- src/app/[locale]/(public)/layout.tsx
+++ src/app/[locale]/(public)/layout.tsx
@@ -1,8 +1,13 @@
+import { unstable_setRequestLocale } from "next-intl/server";
+
 export default function PublicLayout({
   children,
+  params: { locale },
 }: Readonly<{
   children: React.ReactNode;
+  params: { locale: string };
 }>) {
+  unstable_setRequestLocale(locale);
   return (
     <div className="grid min-h-screen grid-rows-[auto_1fr_auto]">
       {children}
